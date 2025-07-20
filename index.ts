2dddd82c5f26a6883d435799c70da420666a5402
Fenny Flametail <fennyflametail@icloud.com>
2025-07-20 17:28:40 -0500

    update build script to preserve header and strip @moz-document

--- index.ts
+++ index.ts
@@ -3,12 +3,20 @@ import postcss from 'postcss';
 import cssnano from 'cssnano';
 import nested from 'postcss-nested';
 
-const css = await fs.readFile('./input.css');
+const css = await fs.readFile('./input.css', 'utf-8');
+const header =
+  css.match(/\/\* ==UserStyle==\n.+\n==\/UserStyle== \*\//s)?.[0] ?? '';
 
 const result = await postcss([nested, cssnano]).process(css, {
   from: './input.css',
   to: './output.css',
 });
 
-fs.writeFile('./output.css', result.css);
-if (result.map) fs.writeFile('./output.css.map', result.map.toString());
+fs.writeFile('./output-stylus.css', header.concat(result.css));
+if (result.map) fs.writeFile('./output-stylus.css.map', result.map.toString());
+
+result.css = result.css.replace(
+  /@-moz-document domain\("bsky\.app"\){(.+)}/s,
+  '$1'
+);
+fs.writeFile('./output-userscripts.css', header.concat(result.css));
