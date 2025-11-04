6f9d4ea6f2ef05faa91e82e24cf52be8c3b24a72
Fenny Flametail <fennyflametail@icloud.com>
2025-11-04 09:02:26 -0600

    2025-11-04.a, updated build script

--- index.ts
+++ index.ts
@@ -5,18 +5,27 @@ import nested from 'postcss-nested';
 
 const css = await fs.readFile('./input.css', 'utf-8');
 const header =
-  css.match(/\/\* ==UserStyle==\n.+\n==\/UserStyle== \*\//s)?.[0] ?? '';
+  css.match(/\/\* ==UserStyle==\n.+\n\/instructions \*\//s)?.[0] ?? '';
 
 const result = await postcss([nested, cssnano]).process(css, {
   from: './input.css',
   to: './output.css',
 });
 
+result.css = result.css.replace(
+  /@-moz-document domain\("bsky\.app"\){(.+)}/s,
+  `
+@-moz-document domain("bsky.app") {
+$1
+}`
+);
+
 fs.writeFile('./output-stylus.css', header.concat(result.css));
 if (result.map) fs.writeFile('./output-stylus.css.map', result.map.toString());
 
+/* output Userscripts for Mac/iOS file with @-moz-document removed */
 result.css = result.css.replace(
-  /@-moz-document domain\("bsky\.app"\){(.+)}/s,
+  /@-moz-document domain\("bsky\.app"\) {(.+)}/s,
   '$1'
 );
 fs.writeFile('./output-userscripts.css', header.concat(result.css));
